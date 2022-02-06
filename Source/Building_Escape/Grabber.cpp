dccec05cb7dc142b81b4504718b9e67315e2c346
Joshua Grimard <jgrimard22@gmail.com>
2022-02-06 13:51:40 -0600

    Refactored code. Optimized for performance. --Joshua

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,100 @@
+// Copyright Joshua Grimard 2022
+
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	FindPhysicsHandle();
+	SetupInputComponent();
+
+}
+
+void UGrabber::SetupInputComponent()
+{
+	PawnInput = GetOwner()->FindComponentByClass<UInputComponent>();
+
+	if (PawnInput)
+	{
+		PawnInput->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		PawnInput->BindAction("Grab", IE_Released, this, &UGrabber::Released);
+	}
+}
+
+void UGrabber::FindPhysicsHandle()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (!PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s is missing a physics handle component"), *GetOwner()->GetName());
+	}
+}
+
+void UGrabber::Grab()
+{
+	GetFirstPhysicsBodyInReach();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber button pressed!"));
+
+}
+
+void UGrabber::Released()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grabber button released"));
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	FHitResult Hit;
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	AActor* ActorHit = Hit.GetActor();
+
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s got hit!"), *ActorHit->GetName());
+	}
+
+	return Hit;
+}
+
